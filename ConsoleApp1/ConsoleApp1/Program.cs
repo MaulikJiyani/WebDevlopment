9dbc97e39c0c8ee3585b2258a4be26db662bf83b
MaulikJiyani <jiyani228@gmail.com>
2018-01-23 01:05:11 +0530

    Console application

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Which country you flied from:");
+            string country = Console.ReadLine();
+
+            Console.Write("How many bag you had with you:");
+            int bag = Console.Read();
+            Console.ReadLine();
+
+            Console.Write("what was the total weight of your luggage :");
+            float weight = Console.Read();
+            float weight1 = weight / bag;
+            Console.ReadLine();
+
+            Console.Write("I flew from " +country+ " having " +bag+" bags with average weight of "+weight1+ " kg");
+
+            Console.ReadKey();
+        }
+    }
+}
